d6a83b8d46e532bd80222a60f01e178430b64873
xxHRUSHIKESHxx <khrushikesh2002@gmail.com>
2023-07-18 23:14:57 +0530

    added summerizer project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import ProjectCard from "./ProjectCard";
 import proImg1 from "../assets/img/project-img1.svg";
 import proImg2 from "../assets/img/project-img2.svg";
 import proImg3 from "../assets/img/project-img3.svg";
+import proImg4 from "../assets/img/project-img4.svg";
 import TrackVisibility from "react-on-screen";
 import "animate.css";
 
@@ -13,21 +14,21 @@ const Projects = () => {
   const projects = [
     {
       title: "Yearly Expenses",
-      description: "Add And See Expenses",
+      description: "Effortlessly keep a record of your expenses with our intuitive interface. Simply add your expenses with details such as the amount spent, date, and category. Our app provides a seamless experience for entering and organizing your financial transactions.",
       imgUrl: proImg1,
       url : "https://yearly-expenses.vercel.app/"
     },
     {
       title: "Youtube Clone",
-      description: "See Vedios, Search Content, Channel Details",
+      description: "With our YouTube clone application, powered by React and Express, you can experience the essence of the world's most popular video-sharing platform right at your fingertips. This comprehensive application enables you to search for videos, explore channels, and enjoy a seamless video streaming experience.",
       imgUrl: proImg2,
       url : "https://cloned-tube.vercel.app/"
     },
     {
-      title: "bussiness start up",
-      description: "Design and  Development",
-      imgUrl: proImg3,
-      url : "./blank.html"
+      title: "Open Ai Article Summerizer",
+      description: "With Vite.js And TailWindCss This Is An Ossume App In Which You Can Read Summary Of Any Link Instead Of Reading That Whole Through Open AI's Cutting Edge Technology",
+      imgUrl: proImg4,
+      url : "https://ai-summerizer-nu.vercel.app/"
     },
     {
       title: "bussiness start up",
